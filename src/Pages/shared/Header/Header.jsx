426cb7fd280bde0db999e579a27fe35d67ab2801
rater7tara <nowshinakteremu005@gmail.com>
2023-05-03 01:58:18 +0600

    carousall added

--- src/Pages/shared/Header/Header.jsx
+++ src/Pages/shared/Header/Header.jsx
@@ -6,14 +6,19 @@ import logo from '../../../assets/logo3.png';
 import './Header.css';
 import { FaHome, FaBlogger, FaGraduationCap } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
+import car5 from '../../../assets/car5.jpg';
+import car2 from '../../../assets/car2.jpg';
+import car4 from '../../../assets/car4.jpg';
+
 
 const Header = () => {
     return (
         <>
-        <Navbar sticky="top" collapseOnSelect expand="lg" bg="light" variant="light">
-            
+            <Navbar sticky="top" collapseOnSelect expand="lg" bg="light" variant="light">
+
                 <Container>
-                <Navbar.Brand href="#home"><img className='logo' src={logo} alt="" /></Navbar.Brand>
+                    <Navbar.Brand href="#home"><img className='logo' src={logo} alt="" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="nav mx-auto">
@@ -31,14 +36,47 @@ const Header = () => {
                 </Container>
             </Navbar>
             <div className='banner-sec'>
-                <div className='banner-div'>
-                <img className='banner' src={'https://img.freepik.com/premium-photo/join-our-team-text-lightbox-composition-white-table-background-business-concept_34933-305.jpg?size=626&ext=jpg&ga=GA1.1.205563488.1680186315&semt=ais'} alt="" />
-                </div>
-                <div className='banner-text'>
-                    <h1>Bon appetit</h1>
-                </div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="car-img d-block"
+                            src={car5}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="car-img d-block "
+                            src={car2}
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="car-img d-block"
+                            src={car4}
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
-            </>
+        </>
     );
 };
 
