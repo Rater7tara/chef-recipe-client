8df7deae41f5bf8ddf4bad19575c66c097cbc3d2
rater7tara <nowshinakteremu005@gmail.com>
2023-05-04 12:36:52 +0600

    login created

--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -14,6 +14,14 @@ const Register = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(name, photo, email, password)
+        createUser(email, password)
+        .then(result => {
+            const createdUser = result.user;
+            console.log(createdUser);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
     return (
         <Container className='w-25 mx-auto'>
